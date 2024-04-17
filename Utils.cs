24157746b7e2e3e09ea9619488977d496112a3b4
Jeremy Behreandt <behreandtjeremy@gmail.com>
2024-04-16 20:30:21 -0500

    Utils Remap Overload function with support for gamma.

--- Utils.cs
+++ Utils.cs
@@ -883,9 +883,10 @@ public static class Utils
     }
 
     /// <summary>
-    /// Maps an input value from an original range to a target range. If the
-    /// upper and lower bound of the original range are equal, will return the
-    /// value unchanged.
+    /// Maps an input value from an original range to a target range.
+    /// Clamps the input value to the original range.
+    /// If the upper and lower bound of the original range are equal, then
+    /// returns the value unchanged.
     /// </summary>
     /// <param name="v">input value</param>
     /// <param name="lbOrig">lower bound of origin range</param>
@@ -907,16 +908,19 @@ public static class Utils
     }
 
     /// <summary>
-    /// Maps an input value from an original range to a target range. If the
-    /// upper and lower bound of the original range are equal, will return
-    /// the value unchanged. If gamma is zero, returns the linear remap of v.
+    /// Maps an input value from an original range to a target range.
+    /// Clamps the input value to the original range.
+    /// If the upper and lower bound of the original range are equal, then
+    /// returns the value unchanged.
+    /// Takes the absolute value of gamma.
+    /// If gamma is zero, returns the linear remap of v.
     /// </summary>
     /// <param name="v">input value</param>
     /// <param name="lbOrig">lower bound of origin range</param>
     /// <param name="ubOrig">upper bound of origin range</param>
     /// <param name="lbDest">lower bound of destination range</param>
     /// <param name="ubDest">upper bound of destination range</param>
-    /// <param name="g">gamma, exponent</param>
+    /// <param name="gamma">gamma</param>
     /// <returns>mapped value</returns>
     public static float Remap(
         in float v,
@@ -924,9 +928,9 @@ public static class Utils
         in float ubOrig = 1.0f,
         in float lbDest = 0.0f,
         in float ubDest = 1.0f,
-        in float g = 1.0f)
+        in float gamma = 1.0f)
     {
-        if (g == 0.0f)
+        if (gamma == 0.0f)
         {
             return Utils.Remap(v, lbOrig, ubOrig, lbDest, ubDest);
         }
@@ -934,14 +938,9 @@ public static class Utils
         double denom = ubOrig - lbOrig;
         if (denom == 0.0d) { return v; }
 
-        // TODO: Test multiple cases.
-        double y = Math.Pow(Math.Min(Math.Max(
-            (v - lbOrig) / denom, 0.0d), 1.0d), 1.0d / g);
-        // if (ubDest < lbDest)
-        // {
-        //     return (float)(lbDest - y * (lbDest - ubDest));
-        // }
-        return (float)(lbDest + y * (ubDest - lbDest));
+        double w = Math.Min((v - lbOrig) / denom, 1.0d);
+        return w > 0.0d ? (float)(lbDest + Math.Pow(w, 1.0d / Math.Abs(gamma))
+            * (ubDest - lbDest)) : 0.0f;
     }
 
     /// <summary>
